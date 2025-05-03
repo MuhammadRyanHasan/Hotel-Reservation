e7d5699a2505900c16fb1ee9a8ab6e15002cd1b6
MuhammadRyanHasan <ryanhasanx@gmail.com>
2025-05-04 02:57:02 +0600

    USER-AUTH

---
+++ JS/UserAuthentication-R/UserAuthentication.js
@@ -0,0 +1,83 @@
+function checkPasswordsMatch() {
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert("Error: Passwords do not match!");
+        return false;
+    }
+
+    return true;
+}
+
+function validatePasswordRules(password) {
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNumber = false;
+    let hasSpecial = false;
+    let specialChars = "!@#$%^&*()_+-=[]{}|;':\",./<>?\\";
+
+    if (password.length < 8) {
+        alert("Error: Password must be at least 8 characters long!");
+        return false;
+    }
+
+    for (let i = 0; i < password.length; i++) {
+        let char = password.charAt(i);
+        let charCode = password.charCodeAt(i);
+
+        if (charCode >= 65 && charCode <= 90) {
+            hasUpper = true;
+        }
+        else if (charCode >= 97 && charCode <= 122) {
+            hasLower = true;
+        }
+        else if (charCode >= 48 && charCode <= 57) {
+            hasNumber = true;
+        }
+        else {
+            for (let j = 0; j < specialChars.length; j++) {
+                if (char === specialChars.charAt(j)) {
+                    hasSpecial = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (!hasUpper) {
+        alert("Error: Password must contain at least one uppercase letter!");
+        return false;
+    }
+    if (!hasLower) {
+        alert("Error: Password must contain at least one lowercase letter!");
+        return false;
+    }
+    if (!hasNumber) {
+        alert("Error: Password must contain at least one number!");
+        return false;
+    }
+    if (!hasSpecial) {
+        alert("Error: Password must contain at least one special character!");
+        return false;
+    }
+
+    return true;
+}
+
+function validateForm() {
+    let password = document.getElementById('password').value;
+
+    if (!checkPasswordsMatch()) {
+        return false;
+    }
+
+    if (!validatePasswordRules(password)) {
+        return false;
+    }
+
+    alert("Registration successful!");
+    return true;
+}
+
+document.getElementById('signup-form').onsubmit = validateForm;
