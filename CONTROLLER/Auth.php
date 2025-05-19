c330b7dfa94acaef9e22286a1087cb1ab2af357a
MuhammadRyanHasan <ryanhasanx@gmail.com>
2025-05-19 10:47:42 +0600

    ~

--- CONTROLLER/Auth.php
+++ CONTROLLER/Auth.php
@@ -3,17 +3,25 @@ session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = trim($_POST['email']);
     $password = trim($_POST['password']);
-    if ($email === 'r@r.com' && $password === 'rrr') {
+    if ($email === 'r@r.com' && $password === 'rrrrr') {
         $_SESSION['authenticated'] = true;
         setcookie('login_status', 'true', time() + 600, '/');
         header('Location: ../VIEW/UserAuthentication-R/temp.php');
         exit();
     } else {
-        header('Location: ../VIEW/UserAuthentication-R/Login.html');
-        exit();
+      header('Location: ../VIEW/UserAuthentication-R/Login.html');
+     exit();
     }
 } else {
-    header('Location: ../VIEW/UserAuthentication-R/Login.html');
-    exit();
+  header('Location: ../VIEW/UserAuthentication-R/Login.html');
+ exit();
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $password = $_POST['password'];
+    
+    if (strlen($password) < 4) {
+        echo "<script>alert('Password must be at least 4 characters long'); window.history.back();</script>";
+        exit();
+    }
 }
 ?>
--- VIEW/UserAuthentication-R/temp.php
+++ VIEW/UserAuthentication-R/temp.php
@@ -9,12 +9,11 @@ if (!isset($_SESSION['authenticated']) && !isset($_COOKIE['login_status']))
 <html>
 <body>
 <br>
-<a href="http://127.0.0.1/Hotel-Reservation/VIEW/BillingSummary-R/BillingSummary.html">BillingSummary</a> <br>
-<a href="https://www.example.com">Visit Example.com</a><br>
-<a href="https://www.example.com">Visit Example.com</a><br>
-<a href="https://www.example.com">Visit Example.com</a><br>
-<a href="https://www.example.com">Visit Example.com</a><br>
-<a href="https://www.example.com">Visit Example.com</a><br>
+<a href="../BillingSummary-R/BillingSymmary.html">BillingSummary</a> <br>
+<a href="../LandingPage-R/LandingPage.html">LandingPage</a><br>
+<a href="../ReviewSystem-R/ReviewSystem.html">ReviewSystem</a><br>
+<a href="../ProfileManagement-R/ProfileManagement.html">ProfileManagement</a><br>
+<a href="../DataExport-R/DataExport.html">DataExport</a><br>
 <a href="https://www.example.com">Visit Example.com</a><br>
 <a href="https://www.example.com">Visit Example.com</a><br>
 <a href="https://www.example.com">Visit Example.com</a><br>
