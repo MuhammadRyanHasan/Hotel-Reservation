4e3452bdfbcb99dd7df92e1f2698ba903a85b37b
MuhammadRyanHasan <ryanhasanx@gmail.com>
2025-05-19 11:08:10 +0600

    ~

--- VIEW/UserAuthentication-R/temp.php
+++ VIEW/UserAuthentication-R/temp.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['authenticated']) && !isset($_COOKIE['login_status']))
 <body>
 <br>
 <a href="../BillingSummary-R/BillingSymmary.php">BillingSummary</a> <br>
-<a href="../LandingPage-R/LandingPage.php">LandingPage</a><br>
+<a href="../LandingPage-R/LandingPage.html">LandingPage</a><br>
 <a href="../ReviewSystem-R/ReviewSystem.php">ReviewSystem</a><br>
 <a href="../ProfileManagement-R/ProfileManagement.php">ProfileManagement</a><br>
 <a href="../DataExport-R/DataExport.php">DataExport</a><br>
