1e34a66f2f8df9e3aae5b2ce81fd270a50bd1d12
MuhammadRyanHasan <ryanhasanx@gmail.com>
2025-05-26 09:49:40 +0600

    AJAX-JSON

--- VIEW/JSON-R/update.php
+++ VIEW/JSON-R/update.php
@@ -1,5 +1,9 @@
 <?php
     $data = $_POST['json'];
     $user = json_decode($data);
-    echo "Updated username: ".$user->username;
+    $response = "<h3>Updated Profile:</h3>";
+    $response .= "<p>Name: ".htmlspecialchars($user->name)."</p>";
+    $response .= "<p>Email: ".htmlspecialchars($user->email)."</p>";
+    $response .= "<p>Password: ".htmlspecialchars($user->password)."</p>";
+    echo $response;
 ?>
